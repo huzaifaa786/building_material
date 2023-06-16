03097cdc640815dc16e429f01b0a598a8495496f
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-16 11:19:31 +0500

    ali

--- database/migrations/2023_05_02_193426_create_products_table.php
+++ database/migrations/2023_05_02_193426_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('category_id');
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('vendor_id');
             $table->string('name');
             $table->string('price');
             $table->string('unit');
