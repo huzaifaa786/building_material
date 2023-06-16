d5a44b71193216e12e824703916b0cc1caecf5d8
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-16 11:44:21 +0500

    ali

--- database/migrations/2023_05_09_193426_create_products_table.php
+++ database/migrations/2023_05_09_193426_create_products_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('category_id');
-            $table->foreignId('vendor_id')->nullable()->constrained('vendors')->onDelete('cascade');
+            $table->foreignId('vendor_id')->nullable();
+            $table->foreign('vendor_id')->references('id')->on('vendors')->onDelete('cascade');
             $table->string('name');
             $table->string('price');
             $table->string('unit');
@@ -25,6 +26,7 @@ return new class extends Migration
             $table->string('description');
             $table->string('discount');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
